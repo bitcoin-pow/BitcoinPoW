133da2cf1a6af5a44c7d2e36ed0b1baa4b01e447
btcw-space <btcw-space@users.noreply.github.com>
2026-09-25 02:13:18 +0000

    Use v26 datadir/conf names and add btcw.space DNS seeds
    Default to ~/.bitcoin-pow and bitcoin-pow.conf so existing v26 installs
    load, and publish seed1-8.btcw.space after the bitcoin-pow.org seeds.

--- test/functional/test_framework/util.py
+++ test/functional/test_framework/util.py
@@ -27,6 +27,7 @@ from collections.abc import Callable
 from typing import Optional, Union
 
 SATOSHI_PRECISION = Decimal('0.00000001')
+BITCOIN_CONF_FILENAME = "bitcoin-pow.conf"
 
 logger = logging.getLogger("TestFramework.utils")
 
@@ -535,7 +536,7 @@ def initialize_datadir(dirname, n, chain, disable_autoconnect=True):
     datadir = get_datadir_path(dirname, n)
     if not os.path.isdir(datadir):
         os.makedirs(datadir)
-    write_config(os.path.join(datadir, "bitcoin.conf"), n=n, chain=chain, disable_autoconnect=disable_autoconnect)
+    write_config(os.path.join(datadir, BITCOIN_CONF_FILENAME), n=n, chain=chain, disable_autoconnect=disable_autoconnect)
     os.makedirs(os.path.join(datadir, 'stderr'), exist_ok=True)
     os.makedirs(os.path.join(datadir, 'stdout'), exist_ok=True)
     return datadir
@@ -604,18 +605,18 @@ def get_temp_default_datadir(temp_dir: pathlib.Path) -> tuple[dict, pathlib.Path
     temp_dir, as well as the complete path it would return."""
     if platform.system() == "Windows":
         env = dict(APPDATA=str(temp_dir))
-        datadir = temp_dir / "Bitcoin"
+        datadir = temp_dir / "Bitcoin-PoW"
     else:
         env = dict(HOME=str(temp_dir))
         if platform.system() == "Darwin":
-            datadir = temp_dir / "Library/Application Support/Bitcoin"
+            datadir = temp_dir / "Library/Application Support/Bitcoin-PoW"
         else:
-            datadir = temp_dir / ".bitcoin"
+            datadir = temp_dir / ".bitcoin-pow"
     return env, datadir
 
 
 def append_config(datadir, options):
-    with open(os.path.join(datadir, "bitcoin.conf"), 'a') as f:
+    with open(os.path.join(datadir, BITCOIN_CONF_FILENAME), 'a') as f:
         for option in options:
             f.write(option + "\n")
 
@@ -623,8 +624,8 @@ def append_config(datadir, options):
 def get_auth_cookie(datadir, chain):
     user = None
     password = None
-    if os.path.isfile(os.path.join(datadir, "bitcoin.conf")):
-        with open(os.path.join(datadir, "bitcoin.conf"), 'r') as f:
+    if os.path.isfile(os.path.join(datadir, BITCOIN_CONF_FILENAME)):
+        with open(os.path.join(datadir, BITCOIN_CONF_FILENAME), 'r') as f:
             for line in f:
                 if line.startswith("rpcuser="):
                     assert user is None  # Ensure that there is only one rpcuser line
